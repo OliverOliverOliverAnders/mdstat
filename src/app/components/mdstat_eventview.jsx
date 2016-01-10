62a45637228c796b361d14a75603a5d7f9e8a7a9
OliverOliverOliverAnders <OliverAnders@olivers-imac-3.speedport_w_724v_09011603_00_018>
2016-01-10 01:10:53 +0100

    filtered list and status in event view

--- src/app/components/mdstat_eventview.jsx
+++ src/app/components/mdstat_eventview.jsx
@@ -8,52 +8,88 @@ import {headlineStyle} from '../style';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 
+
+import CheckIcon from 'material-ui/lib/svg-icons/navigation/check';
+import ErrorIcon from 'material-ui/lib/svg-icons/alert/error';
+import WarnIcon from 'material-ui/lib/svg-icons/alert/warning';
+import colors from 'material-ui/lib/styles/colors';
+
 let Store=require('../store.js');
 
 let MDStatEventView = React.createClass({
   getInitialState:function(){
-    return {approved:this.props.event.approved};
+    return {filterForErrors:false};
   },
 
-     createInfoList: function(info){
+  getStatusIcon:function(){
+    if (this.props.event.status==="success"){
+      return <CheckIcon color={colors.green200} label="Success"/>;
+    }
+    else if (this.props.event.status==="error"){
+      return <ErrorIcon color={colors.red200} label="Error"/>;
+    }
+    else{
+      return <WarnIcon color={colors.lime200} label="Warning"/>;
+    }
+  },
+  createInfoList: function(info){
+    let mthis=this;
+    return info.map(
+      function(entry)
+      {
+        if(~(entry.indexOf("ERROR:"))){
+          return <ListItem primaryText={entry} leftIcon=<ErrorIcon/>/>;
+        }else if(!mthis.state.filterForErrors){
+          return <ListItem primaryText={entry} leftIcon=<CheckIcon/>/>;
+        }
+      }
+    );
+  },
+  handleCheckApprove:function(event,checked){
+    Store.trigger("toggleApproved",this.props.event,checked)
 
-       return info.map(
-         function(entry)
-         {
-           return <ListItem primaryText={entry}/>;
-         }
-       );
-    },
-handleCheckApprove:function(event,checked){
-  Store.trigger("toggleApproved",this.props.event,checked)
 
+  },
+  handleCheckFilterForErrors:function(event,checked){
+    this.setState({filterForErrors:checked});
+  },
 
-},
-
-     render: function(){
+  render: function(){
 
     let infoList=this.createInfoList(this.props.event.info);
-          /* get Data from props */
-      let approvedString=this.props.event.approved?"approved":"not approved";
-     return <Paper id="blbl" style={headlineStyle} zDepth={1}>
-        <div className="row">
-          <div className="col-1-2">ID: {this.props.event._id} </div>
-          <div className="col-1-2">
-            <Checkbox
-             name="approved"
-             checked={this.props.event.approved}
-             label="approved"
-             onCheck={this.handleCheckApprove}/>
-         </div>
+    /* get Data from props */
 
-       </div>
-       <List>
-         {infoList}
-       </List>
+    return <Paper id="blbl" style={headlineStyle} zDepth={1}>
+      <div className="row">
+        <div className="col-1-4">ID: {this.props.event._id} </div>
+        <div className="col-1-4">
+          <Checkbox
+            name="approved"
+            checked={this.props.event.approved}
+            label="approved"
+            onCheck={this.handleCheckApprove}/>
+        </div>
+        <div className="col-1-4">
+          {this.getStatusIcon()} {this.props.event.status}
+        </div>
+        <div className="col-1-4">
+          <Checkbox
+            name="Show errors only"
+            checked={this.state.filterForErrors}
+            label="Show errors only"
+            onCheck={this.handleCheckFilterForErrors}
+            />
+        </div>
+        
+
+      </div>
+      <List>
+        {infoList}
+      </List>
 
 
-         </Paper>;
-         },
-     });
+    </Paper>;
+  },
+});
 
 export default MDStatEventView;
